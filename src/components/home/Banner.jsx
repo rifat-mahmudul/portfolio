b1075ea37ed31129d91b9eae64e1dcaf5d6763a3
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-11 18:56:09 +0600

    custom cursor done

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import BannerText from "./BannerText";
 import Rifat from "../../assets/Rifat.png";
 import { useEffect } from "react";
-import SplashCursor from "../Animation/SplashCursor";
+// import SplashCursor from "../Animation/SplashCursor";
 
 const Banner = () => {
 
@@ -71,10 +71,10 @@ const Banner = () => {
     return (
         <section id="home" className="relative overflow-hidden mt-20 sm:mt-14 lg:mt-5">
 
-            <SplashCursor></SplashCursor>
+            {/* <SplashCursor></SplashCursor> */}
 
             {/* Custom Cursor */}
-            <div id="custom-cursor" className="cursor-default absolute w-[15px] h-[15px] bg-pink-600 rounded-full pointer-events-none z-50 transition-all duration-150"></div>
+            {/* <div id="custom-cursor" className="cursor-default absolute w-[15px] h-[15px] bg-pink-600 rounded-full pointer-events-none z-50 transition-all duration-150"></div> */}
             
             <div className="min-h-screen flex flex-col md:flex-row justify-between items-center max-w-[90%] xl:max-w-[1050px] mx-auto">
                 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router";
 import AppRoutes from './Routes/Routes';
+import AnimatedCursor from "react-animated-cursor"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <BrowserRouter>
         <AppRoutes></AppRoutes>
       </BrowserRouter>
+      <AnimatedCursor 
+      innerSize={10}
+      outerSize={36}
+      innerScale={0.7}
+      outerScale={2}
+      color="255, 0, 127"
+      outerAlpha={0.5}
+      innerStyle={{
+          backgroundColor: "#FF007F",
+          border: "none",
+      }}
+      outerStyle={{
+          backgroundColor: "transparent",
+          border: "2px solid #FF007F", 
+      }}
+      style={{
+        mixBlendMode: "difference",
+      }}
+      showSystemCursor={true}
+      />
   </StrictMode>,
 )
